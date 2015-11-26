34e3b26edb3a0c215e0cb391733e6ce4e113d5f1
laielvin <elvin_lai@hotmail.com>
2015-11-25 23:51:23 -0800

    Update Rule3.java

--- src/Rule3.java
+++ src/Rule3.java
@@ -1,1 +1,22 @@
-
+public class Rule2 extends Rule
+{
+  // 3) Check each word of the sentence and make sure the character between them is a single space character, 
+  // if it isn’t return a true value for rule 3
+  
+	public boolean check(String sentence)
+	{
+		if (sentence.endsWith(".") || sentence.endsWith("?") || sentence.endsWith("!"))
+		{
+			return false;
+		}
+		else
+		{
+		return true;
+		}
+	}
+	
+	public String message()
+	{
+		return "error2";
+	}
+}
