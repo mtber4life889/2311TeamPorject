12e53298cbbed12773d9b3e1dec13e89ecea390a
laielvin <elvin_lai@hotmail.com>
2015-11-27 03:41:51 -0800

    I will work on

---
+++ src/Rule8.java
@@ -0,0 +1,2 @@
+//8) Check the sentence for an apostrophe character and if there is one, 
+check the word it's in against a list of contracted words and if it doesn’t match any return a true value for rule 8
