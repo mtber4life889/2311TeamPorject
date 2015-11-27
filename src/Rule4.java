8d718e2b534af51da032b71723ad9698c48c9dee
mtber4life889 <curtis_chippeway@hotmail.com>
2015-11-26 16:45:58 -0800

    quick comment on rule 4

--- src/Rule4.java
+++ src/Rule4.java
@@ -2,6 +2,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Rule4 extends Rule 
 {
+	//This doesn't work at all i know it doesn't but I think it's close to what we need
+	//If we can figure out this class we can do the other rules that use this same logic
 	Pattern SL_Pattern = Pattern.compile("\\W");
 	Matcher SC_M = SL_Pattern.matcher("\\W");
 	public boolean check(String sentence)
