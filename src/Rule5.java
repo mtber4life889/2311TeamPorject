31b6eb609839b196a7b6c50be76a64bd514c9c24
laielvin <elvin_lai@hotmail.com>
2015-11-27 03:34:00 -0800

    Changed Rule to double negative

--- src/Rule5.java
+++ src/Rule5.java
@@ -1,4 +1,4 @@
 
 /* Code that will find if these variables are repeated twice. 
 
-// don't, not, didn't, never, no, nothing, nowhere, non   
+// never, no, nothing, nowhere, non, not, n't, none     
