329d8115ae6b0055c7b88886847ba277a68591b9
laielvin <elvin_lai@hotmail.com>
2015-11-27 03:48:07 -0800

    Changed rule to whom vs who

--- src/Rule9.java
+++ src/Rule9.java
@@ -1,7 +1,10 @@
 Rule. Use this he/him method to decide whether who or whom is correct:
 
-Who/Whom wrote the letter?
-He wrote the letter. Therefore, who is correct.
+/*Who/Whom wrote the letter?
+  He wrote the letter. Therefore, who is correct.
 
-Who/Whom should I vote for?
-Should I vote for him? Therefore, whom is correct.
+  Who/Whom should I vote for?
+  Should I vote for him? Therefore, whom is correct.
+*/
+
+// 9) Check if the sentence for who and he is present or whom and him.  If both does not occur, return false.
