ff66fba4abe4bcc8ecae56e92fb8cd8ec4e14948
laielvin <elvin_lai@hotmail.com>
2015-11-27 03:38:49 -0800

    This works...sorta

--- src/Rule4.java
+++ src/Rule4.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Rule4 extends Rule 
 {
-	//This doesn't work at all i know it doesn't but I think it's close to what we need
+/*	//This doesn't work at all i know it doesn't but I think it's close to what we need
 	//If we can figure out this class we can do the other rules that use this same logic
 	Pattern SL_Pattern = Pattern.compile("\\W");
 	Matcher SC_M = SL_Pattern.matcher("\\W");
@@ -17,6 +17,21 @@ public class Rule4 extends Rule
 		return false;
 		}
 	}
+
+	
+		/* Still working on the matcher. 
+		
+		Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher("I am a string");
+		boolean b = m.find();
+		if (b)
+   			System.out.println("There is a special character in my string");
+   		*/
+   		
+		public boolean check(String sentence) 
+		{
+		return (sentence == null) ? false : sentence.matches("[^A-Za-z0-9 ]");
+		}
 	
 	public String message()
 	{
