9548977fdf6c76e74617e3c3d549da327f6bc011
laielvin <elvin_lai@hotmail.com>
2015-11-27 16:28:02 -0800

    Update Rule5.java

--- src/Rule5.java
+++ src/Rule5.java
@@ -1,4 +1,26 @@
+import java.util.regex.Pattern;
 
-/* Code that will find if these variables are repeated twice. 
+public class Rule5 extends Rule 
+{
+	public boolean check(String sentence)
+	{
+		
+		String[] Neg = { "n't", "not", "never", "no", "nothing", "nowhere", "non"};     
+		String[] DoubleN = sentence.split(sentence); 
+		// working on how to match variables from variables
+		
+		if(Neg == DoubleN) 
+		{
+			return false; 
+		}
+		return false;
+	
+	}
+		
+		public String message()
+		{
+			return "rule 5 error";
+		
+		}
+}
 
-// never, no, nothing, nowhere, non, not, n't, none     
