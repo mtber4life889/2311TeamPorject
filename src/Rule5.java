41352ebb4f8bb17f21e51b6185e08db16023d9ae
laielvin <elvin_lai@hotmail.com>
2015-11-30 18:09:26 -0800

    Update Rule5.java

--- src/Rule5.java
+++ src/Rule5.java
@@ -1,22 +1,22 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Rule5 extends Rule 
 {
 	public boolean check(String sentence)
 	{
-		
-		String[] Neg = { "n't", "not", "never", "no", "nothing", "nowhere", "non"};     
-		String[] DoubleN = sentence.split(sentence); 
-		// working on how to match variables from variables
-		
-		if(Neg == DoubleN) 
-		{
-			return false; 
-		}
-		return false;
+		  
+			   	 Pattern Neg = Pattern.compile("\\b(n't|not|never|no|nothing|nowhere|non){2}\\b");  
+			   	 Matcher Matcher = Neg.matcher(sentence); 
+			     if (Matcher.find()){
+			     {
+			    	return true; 
+			     }
+			     return false; 
 	
 	}
 	public String message()
 		{
-			return "rule 5 error";
+			return "rule 5 error"; 
 		}
 }
-
