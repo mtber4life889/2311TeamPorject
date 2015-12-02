60ce5f298b27014b457f9d252184c5b0a9e0aa78
laielvin <elvin_lai@hotmail.com>
2015-12-01 20:47:01 -0800

    Update Rule8.java

--- src/Rule8.java
+++ src/Rule8.java
@@ -1,2 +1,37 @@
-//8) Check the sentence for an apostrophe character and if there is one, 
-check the word it's in against a list of contracted words and if it doesn’t match any return a true value for rule 8
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Rule8 extends Rule 
+{
+	public boolean check(String sentence)
+	{
+		 if (sentence.matches("\\s(,)."))
+		 {
+			 Pattern Conj = Pattern.compile("\\b(and|but|for|nor|or|so|yet)\\b");  
+			 Matcher match = Conj.matcher(sentence); 
+			 		if (match.find())
+			 		{
+			 				return false; 
+			 		}
+			 		else return true;
+			 		
+		
+		 }
+		 else return false; 
+	}
+	
+	public String message()
+		{
+			return "rule 8 error";
+		}
+	
+}
+
+
+
+/* Takes string 
+ * Split string into array elements 
+ * Compare array conj and split array to find single element matches 
+ * Out put true if matches
+ * Out put error, conj not present
+ */
