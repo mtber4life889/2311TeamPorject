41a08129d7b6860625fec9836c0fe68b0242921f
mtber4life889 <curtis_chippeway@hotmail.com>
2015-12-05 07:44:06 -0800

    RULE 2 WORKS I DIDNT EVEN CHANGE ANYTHING

--- src/tMain.java
+++ src/tMain.java
@@ -10,10 +10,10 @@ public class tMain {
 		String sentence = sc.nextLine();
 		
         GrammarChecker checker = new GrammarChecker();
-        //checker.addRule(new Rule1()); this works
-        //checker.addRule(new Rule2()); this works
-        //checker.addRule(new Rule3()); this works
-        checker.addRule(new Rule4());
+        checker.addRule(new Rule1()); //this works
+        checker.addRule(new Rule2()); //this works, seriously no joke
+        //checker.addRule(new Rule3()); 
+        //checker.addRule(new Rule4());
         //checker.addRule(new Rule5()); this somewhat works but practical application would be impossible
         //checker.addRule(new Rule6());
         //checker.addRule(new Rule8());
