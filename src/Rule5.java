c95570b03dbd12d2932467bcd19bf4acdeac1fc6
laielvin <elvin_lai@hotmail.com>
2015-12-06 16:37:30 -0800

    Update Rule5.java

--- src/Rule5.java
+++ src/Rule5.java
@@ -1,35 +1,23 @@
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class Rule5 extends Rule 
 {
 	public boolean check(String sentence)
 	{
-		  
-			   	 Pattern Neg = Pattern.compile(".*(n't|not|never|no|nothing|nowhere|non).*{2,3}");  
-			   	 Matcher Matcher = Neg.matcher(sentence); 
-			   	 
-			     if (Matcher.find())
-			     {
-			    	return false; 
-			     }
-			     else
-			     {
-			    	 return true;
-			     }
-			    
-	}
-			     
+			Pattern pattern = Pattern.compile("not|Not(\\s)not|Not");
+	        if (pattern.matcher(sentence).find())
+	        {
+	            return false;
+	        }
+			
+		    return true;
+		}
+	
 	public String message()
 		{
 			return "rule 5 error"; 
 		}
 }
 
-/* Takes string 
- * Split string into array elements 
- * Compare array Neg and split array to find a two or more element match
- * Out put true if matches
- * Out put error, Neg not present
- */
 
 
